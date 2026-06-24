89858f6622cf4efda42a78a59bf85472c92cf07b
kristhian <lidsmg3@gmail.com>
2026-06-24 16:30:51 -0500

    Creacion Metodo establecerConexion

--- src/main/java/com/mycompany/proyecto/ConexionMysql.java
+++ src/main/java/com/mycompany/proyecto/ConexionMysql.java
@@ -4,15 +4,37 @@
  */
 package com.mycompany.proyecto;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Usuario
  */
 public class ConexionMysql {
     private static final String USUARIO = "root";
-    private static final String CONTRASEÑA = "";
+    private static final String CONTRASENA = "";
     private static final String BD = "biblioteca";
     private static final String IP = "localhost";
     private static final String PUERTO = "3306";
+    
+    private static final String URL = "jdbc:mysql://\" + IP +\":\" + PUERTO + \"/\" + BD + \"?useSSL=false&serverTimezone=UTC";
+    
+    private Connection conexion;
+    
+    public Connection establecerConexion(){
+        
+        try{
+            conexion = DriverManager.getConnection(URL,USUARIO,CONTRASENA);
+            
+            System.out.println("Conexion Exitosa a MySQL");
+            
+        }catch(SQLException e){
+            System.out.println("ERROR al conectar"+e.getMessage());
+        }
+        return conexion;
+    }
+            
     
 }
