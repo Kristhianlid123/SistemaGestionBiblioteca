5483c1418cf93db60c1ed97a054f552d59b5eb0b
kristhian <lidsmg3@gmail.com>
2026-07-02 10:18:46 -0500

    Creacion TestPrestamo

---
+++ src/main/java/DAO/TestPrestamo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Date;
+
+public class TestPrestamo {
+
+    public static void main(String[] args) {
+
+        PrestamoDAO insert = new PrestamoDAO();
+
+        insert.guardarPrestamo(
+                1,
+                1,
+                Date.valueOf("2026-07-02"),
+                Date.valueOf("2026-07-09"),
+                "Prestado"
+        );
+
+    }
+
+}
